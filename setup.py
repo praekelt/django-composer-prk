ca96d3b63749b6d0ffd57902398e9630288532ed
Johan Beyers <johan@juizi.com>
2016-11-07 12:23:04 +0200

    Fix setup name

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="jmbo-listing",
+    name="jmbo-composer",
     version="0.1",
-    description="A listing is a stored search that can be rendered in a certain style.",
+    description="Page composer for Jmbo/Mobius",
     long_description = open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
     author="Praekelt Consulting",
     author_email="dev@praekelt.com",
     license="BSD",
-    url="https://github.com/praekelt/jmbo-listing",
+    url="https://github.com/praekelt/jmbo-composer",
     packages = find_packages(),
     install_requires = [
         # Redundant. Pip handles this now.
