7ae4d54b08a253111cab26a6015a8b33fc62bd7f
Johan Beyers <johan@juizi.com>
2016-11-10 10:38:53 +0200

    Admin inlines added

--- composer/admin.py
+++ composer/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+import nested_admin
 
-# Register your models here.
+from .models import Page, Row, Column, Tile
+
+
+class TileInline(nested_admin.NestedTabularInline):
+    model = Tile
+    sortable_field_name = "position"
+    extra = 1
+
+
+class ColumnInline(nested_admin.NestedTabularInline):
+    model = Column
+    sortable_field_name = "position"
+    inlines = [TileInline]
+    extra = 1
+
+
+class RowInline(nested_admin.NestedTabularInline):
+    model = Row
+    sortable_field_name = "position"
+    inlines = [ColumnInline]
+    extra = 1
+
+
+class PageAdmin(nested_admin.NestedModelAdmin):
+    inlines = [RowInline]
+
+admin.site.register(Page, PageAdmin)
