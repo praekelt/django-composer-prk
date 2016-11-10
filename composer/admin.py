7074434eb7ff63e72e4e81377001f85e3760d7bd
Johan Beyers <johan@juizi.com>
2016-11-10 10:59:16 +0200

    Cleaner UI

--- composer/admin.py
+++ composer/admin.py
@@ -7,24 +7,25 @@ from .models import Page, Row, Column, Tile
 class TileInline(nested_admin.NestedTabularInline):
     model = Tile
     sortable_field_name = "position"
-    extra = 1
+    extra = 0
 
 
 class ColumnInline(nested_admin.NestedTabularInline):
     model = Column
     sortable_field_name = "position"
     inlines = [TileInline]
-    extra = 1
+    extra = 0
 
 
 class RowInline(nested_admin.NestedTabularInline):
     model = Row
     sortable_field_name = "position"
     inlines = [ColumnInline]
-    extra = 1
+    extra = 0
 
 
 class PageAdmin(nested_admin.NestedModelAdmin):
     inlines = [RowInline]
+
 
 admin.site.register(Page, PageAdmin)
