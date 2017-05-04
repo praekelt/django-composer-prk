ff8dfb99600b6f1c3ac334e2a3339e36260c97a6
AltusBarry <AltusBarry@users.noreply.github.com>
2017-05-04 13:59:43 +0200

    Added comments, pep8 changes, fixed overriding key issue

--- composer/admin.py
+++ composer/admin.py
@@ -39,15 +39,42 @@ class TileInlineForm(forms.ModelForm):
         self.fields["style"].widget = forms.widgets.Select(choices=styles)
 
     def get_existing_styles(self):
+        # Get a list of all the availible templates in the project.
         template_dirs = app_directories.get_app_template_dirs("templates")
+
         template_dict = {}
+        # Traverse all the directories within the tempalte directories of all
+        # availible apps.
         for app_templates in template_dirs:
             for path, dirnames, filenames in os.walk(app_templates):
+
+                # Composer template tags expect the templates to live within
+                # the app inclusion tags, so a good area to start ignoring any
+                # templates that won't match.
                 if filenames and "inclusion_tags" in path:
                     for filename in filenames:
-                        if len(filename.replace(".html", "").split("_")) > 1:
-                            template_dict[filename.split("_")[0]] = { "path": os.path.join(path, filename), "filename": filename }
-                            test_list = template_dict.keys()
+
+                        # The naming convention that the composer tags expect
+                        # is modelnam_style.html. If the template name has no
+                        # underscores it won't ever be valid.
+                        if len(filename.split("_")) > 1:
+
+                            # Grab the first part of the template name as the
+                            # key, useful to easily ignore templates that
+                            # contain underscores but not the modelname at the
+                            # start. We check against actual model names later.
+                            key = filename.split("_")[0]
+                            data = {
+                                "path": os.path.join(path, filename),
+                                "filename": filename
+                            }
+
+                            # There will more than likely be multiple templates
+                            # with the same base key, so we add them to a list
+                            # for iteration later on.
+                            if template_dict.get(key, None) is None:
+                                template_dict[key] = []
+                            template_dict[key].append(data)
 
         styles_dict = {}
         for model in apps.get_models():
@@ -55,13 +82,32 @@ class TileInlineForm(forms.ModelForm):
             app_label = model._meta.app_label
             template_data = template_dict.get(model_name)
             if template_data:
-                filename = template_data["filename"]
-                path = template_data["path"]
-                if "%s/inclusion_tags/%s_" % (app_label, model_name) in path:
-                    pattern = re.compile(("%s_|.html" % (model_name)))
-                    style = pattern.sub("", filename)
-                    if style not in styles_dict.keys():
-                        styles_dict[style] = style.capitalize().replace("_", " ")
+                for template in template_data:
+                    filename = template["filename"]
+                    path = template["path"]
+
+                    # Further exclusions based on the composer template tag's
+                    # expectations.
+                    # <app_label>/inclusion_tags/<model_name>_<style>
+                    if "%s/inclusion_tags/%s_" % (app_label, model_name) in path:
+
+                        # Remove the file extension and model name to get only
+                        # the style.
+                        pattern = re.compile(("%s_|.html" % (model_name)))
+                        style = pattern.sub("", filename)
+
+                        # We add the styles to a dictionary, so we can easily
+                        # make sure we don't have duplicate styles and convert
+                        # it to a list of tuples.
+                        if style not in styles_dict.keys():
+
+                            # Try to make the values we display a bit more
+                            # admin friendly.
+                            styles_dict[style] = style.capitalize().replace(
+                                "_", " "
+                            )
+
+        # Return the list of tuples containing the found styles.
         return [(k, v) for k, v in styles_dict.iteritems()]
 
 
